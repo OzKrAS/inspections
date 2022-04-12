3cf2cb177da68da2ea55569c1e1e8b85fd78ae3f
Andres Cupa <jamendoza23@misena.edu.co>
2022-04-12 01:33:11 -0500

    update web

--- routes/web.php
+++ routes/web.php
@@ -151,3 +151,6 @@ Route::group(['middleware'=>['auth']],function(){
 });
 
 //Route::get('/home', 'HomeController@index')->name('home');
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
