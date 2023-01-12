3f1ad2335be9fa81344501bfec63b91dea18084e
OzKrTec <ossierrae@sena.edu.co>
2023-01-12 13:07:24 -0500

    add migration detDatFlaps

---
+++ database/migrations/2023_01_12_125444_detail_fisher_aut_det_fl.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DetailFisherAutDetFl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+            Schema::create('detail_fisher_aut_det_flaps', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_fisheryAut');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+         Schema::dropIfExists('detail_fisher_aut_det_flaps');
+    }
+}
