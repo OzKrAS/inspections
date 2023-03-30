a99151deb0972216aaf2184731290a98525128f5
OzKr <oscar.sierra@misena.edu.co>
2023-03-30 09:59:10 -0500

    correction fichery tuna

--- app/Http/Controllers/DataJsonController.php
+++ app/Http/Controllers/DataJsonController.php
@@ -2285,7 +2285,7 @@ class DataJsonController extends Controller
                         "placeHolder"=>"PLACE HOLDER INPUT",
                         "id"=>"ZoneFisher",
                         "cons"=>8,
-                        "data"=> $docks
+                        "data"=> $fishery
                     ],
                     [
                         "type"=>3,
