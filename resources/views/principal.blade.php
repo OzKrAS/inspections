d2080cbd6909dd760caa0a0ed4f2466f9dea5697
Daniel Arcos <danielarcos2011@gmail.com>
2023-05-05 17:29:42 -0500

    Cambiando ruta perfil en el menu de cuenta usuario archivo principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -68,10 +68,11 @@
                     <a class="dropdown-item" href="{{ route('logout') }}" 
                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <i class="fa fa-lock"></i> Cerrar sesión</a>
-                    <a class="dropdown-item" href="{{ route('logout') }}" 
+                    {{-- <a class="dropdown-item" href="{{ route('logout') }}" 
                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="fa fa-user"></i> Perfil</a> --}}
+                    <a class="dropdown-item" href="#">
                     <i class="fa fa-user"></i> Perfil</a>
-
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         {{ csrf_field() }}
                     </form>
