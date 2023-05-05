220a5b7a0f280979ea5b006d00ef808a5e203833
Daniel Arcos <danielarcos2011@gmail.com>
2023-05-05 17:14:17 -0500

    Modificando arrivals.vue

--- resources/assets/js/components/Arrivals.vue
+++ resources/assets/js/components/Arrivals.vue
@@ -2634,10 +2634,10 @@ getArrivalImg() {
 
           let suma1 = 0;
           datos.forEach(e => {
-            suma1 += JSON.parse(e.capture1);
-            this.sumatotalcapturaobj = suma1;
             console.log(e.capture1);
+            suma1 += JSON.parse(e.capture1);
             console.log(suma1);
+            this.sumatotalcapturaobj = JSON.parse(suma1);
             console.log(this.sumatotalcapturaobj);
           });
           
