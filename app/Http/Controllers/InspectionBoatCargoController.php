f86fdeb6af3aeb1863ee32e602c044ec9aff7049
Daniel Arcos <danielarcos2011@gmail.com>
2023-08-14 10:06:08 -0500

    cambios inspection cargo boat

--- app/Http/Controllers/InspectionBoatCargoController.php
+++ app/Http/Controllers/InspectionBoatCargoController.php
@@ -147,7 +147,7 @@ class InspectionBoatCargoController extends Controller
         $array = array(
             'res' => true,
             'message' => 'Registro guardado exitosamente',
-            'id' => $inspections->id
+            // 'id' => $inspections->id
         );
 
         return response()->json($array,201);
