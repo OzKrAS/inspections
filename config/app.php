5aa90825da7edfb6e3ce156b447080bdf62d2df2
Daniel Arcos <danielarcos2011@gmail.com>
2023-08-15 17:48:49 -0500

    mod configapp

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
 
-        Yajra\Datatables\DatatablesServiceProvider::class,
+        // Yajra\Datatables\DatatablesServiceProvider::class,
     ],
 
     /*
@@ -230,7 +230,7 @@ return [
         'PDF' => Barryvdh\DomPDF\Facade::class,
         'Image' => Intervention\Image\Facades\Image::class,
 
-        'Datatables' => 'Yajra\Datatables\Facades\Datatables',
+        // 'Datatables' => 'Yajra\Datatables\Facades\Datatables',
     ],
 
 ];
