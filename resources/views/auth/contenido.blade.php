b9c557ac0fa31dbafbc8d4bab4d330607ac57689
Daniel Arcos <danielarcos2011@gmail.com>
2023-08-15 15:54:31 -0500

    versionamiento estilos

--- resources/views/auth/contenido.blade.php
+++ resources/views/auth/contenido.blade.php
@@ -15,7 +15,7 @@
 
     <title>Sistema Inspecciones</title>
 
-    <link href="{{ asset('css/theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/theme.css?version=1.2') }}" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@150&display=swap" rel="stylesheet">
