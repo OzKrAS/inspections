10730b5f1b054d5bb8f7390d7f4cfcad321b5415
Daniel Arcos <danielarcos2011@gmail.com>
2023-08-16 17:46:55 -0500

    Mod arrivals vue, select boats url

--- resources/assets/js/components/Arrivals.vue
+++ resources/assets/js/components/Arrivals.vue
@@ -1897,7 +1897,8 @@ export default {
     },
     selectBoats() {
       let me = this;
-      var url = "/selectboats?type=0";
+      // var url = "/selectboats?type=0";
+      var url = "/selectboats";
       axios.get(url).then(function (response) {
         var respuesta = response.data;
         me.arrayBoat = respuesta.boat;
