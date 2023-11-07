3da206d7ddc9e8d1d42a040f966372e76eb5c964
darcos3 <danielarcos2011@gmail.com>
2023-11-07 12:00:50 -0500

    mod zarpe header print

--- resources/assets/js/components/Zarpe.vue
+++ resources/assets/js/components/Zarpe.vue
@@ -1935,7 +1935,7 @@ export default {
         var logo = new Image();
         // logo.src = '/img/logoAUNAP.png';
         logo.src = '/img/tituloImpZarpe.png';
-        doc.addImage(logo, 'png', 20, 10, 330, 215);
+        doc.addImage(logo, 'png', 20, 10, 130, 55);
         doc.text("FORMATO DE AUTORIZACIÓN DE ZARPE", 60, 20);
         // doc.text(`FORMATO ACTA DE DONACIÓN ${variable} , otro texto si necesita mas variables ${otra}`, 65, 60);
         doc.setFont("arial");
