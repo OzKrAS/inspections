6605f54d03daa54423a1c03d0aaf9c6edcb5b51c
darcos3 <danielarcos2011@gmail.com>
2023-11-16 11:19:38 -0500

    mod donation certificado - target pdf

--- resources/assets/js/components/DonationCertificates.vue
+++ resources/assets/js/components/DonationCertificates.vue
@@ -1358,6 +1358,7 @@ en presencia de la autoridad competente.
                   //   ...item,
                   //   deleted : false,
                   // }
+                  console.log("ITEMS => ", item);
                   rows = [
                     {
                       "nomCientifico": item.nameScientific,
